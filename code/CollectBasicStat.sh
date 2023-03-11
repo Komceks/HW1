1ee0fc440d92ca890684e94a840bd6cfc05e67fa
marty <marty@LAPTOP-22GF3ARH>
2023-03-11 02:17:23 +0200

    -.-

--- code/CollectBasicStat.sh
+++ code/CollectBasicStat.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 #Prints the number of sequences in reference genome
-grep -c ">" Mus_musculus.NCBIM37.67.dna.toplevel.fa
+grep -c ">" ~/HW1/references/Mus_musculus.NCBIM37.67.dna.toplevel.fa
 
 #Calculates and prints the number of reads in each sample
 
 #46388681 reads
-echo $(cat SRR8985047.fastq|wc -l)/4|bc
+echo $(zcat ~/HW1/inputs/SRR8985047.fastq.gz|wc -l)/4|bc
 #40881658 reads
-echo $(cat SRR8985048.fastq|wc -l)/4|bc
+echo $(cat ~/HW1/inputs/SRR8985048.fastq.gz|wc -l)/4|bc
 #44958444 reads
-echo $(cat SRR8985051.fastq|wc -l)/4|bc
+echo $(cat ~/HW1/inputs/SRR8985051.fastq.gz|wc -l)/4|bc
 #49424572 reads
-echo $(cat SRR8985052.fastq|wc -l)/4|bc
+echo $(cat ~/HW1/inputs/SRR8985052.fastq.gz|wc -l)/4|bc
 
 
 #Calculates protein-coding genes
-cat "Mus_musculus.NCBIM37.67.gtf" | grep -F 'gene' | grep -c 'gene_biotype "protein_coding"'
+cat ~/HW1/references/Mus_musculus.NCBIM37.67.gtf | grep -F 'gene' | grep -c 'gene_biotype "protein_coding"'
--- code/getReferences.sh
+++ code/getReferences.sh
@@ -2,17 +2,17 @@
 #Mus Musculus mm9 https://www.ncbi.nlm.nih.gov/data-hub/genome/GCF_000001635.18/
 
 #Download Genome reference FASTA
-wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/dna/Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz"
-gzip -d Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz
+wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/dna/Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz" -P ~/HW1/references/
+gzip -d ~/HW1/references/Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz
 #Download transcriptome reference FASTA.gz
-wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/cdna/Mus_musculus.NCBIM37.67.cdna.all.fa.gz"
-gzip -d Mus_musculus.NCBIM37.67.cdna.all.fa.gz
+wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/cdna/Mus_musculus.NCBIM37.67.cdna.all.fa.gz" -P ~/HW1/references/
+gzip -d ~/HW1/references/Mus_musculus.NCBIM37.67.cdna.all.fa.gz
 #download gtf file
-wget "https://ftp.ensembl.org/pub/release-67/gtf/mus_musculus/Mus_musculus.NCBIM37.67.gtf.gz"
-gzip -d Mus_musculus.NCBIM37.67.gtf.gz
+wget "https://ftp.ensembl.org/pub/release-67/gtf/mus_musculus/Mus_musculus.NCBIM37.67.gtf.gz" -P ~/HW1/references/
+gzip -d ~/HW1/references/Mus_musculus.NCBIM37.67.gtf.gz
 
 #Download .SRA files
-prefetch SRR8985047 SRR8985048 SRR8985051 SRR8985052
+prefetch -O ~/HW1/inputs/ SRR8985047 SRR8985048 SRR8985051 SRR8985052
 
 #Convert to FASTQ files
-fastq-dump --gzip SRR8985047 SRR8985048 SRR8985051 SRR8985052
+fastq-dump --gzip -O ~/HW1/inputs/ SRR8985047 SRR8985048 SRR8985051 SRR8985052
