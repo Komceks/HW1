9175467019392c1f812c67394ddfedbb0bceca3e
marty <marty@LAPTOP-22GF3ARH>
2023-03-11 19:25:37 +0200

    Final script update for Part 1

--- code/MoreCalculations.sh
+++ code/MoreCalculations.sh
@@ -4,6 +4,6 @@
 echo "(46388681+40881658+44958444+49424572)/4" | bc
 
 #Calculates chromosomes
-cat Mus_musculus.NCBIM37.67.gtf | grep -v '^#' | cut -f1 | sort -n | uniq | wc -l
+cat ~/HW1/references/Mus_musculus.NCBIM37.67.gtf | grep -v '^#' | cut -f1 | sort -n | uniq | wc -l
 
 
